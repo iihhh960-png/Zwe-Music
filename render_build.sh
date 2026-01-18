aca93f387f8363a428532860c7c34606b88a1ec6
iihhh960-png <iihhh960@gmail.com>
2026-01-18 16:02:17 +0630

    Update render_build.sh

--- render_build.sh
+++ render_build.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# လိုအပ်တဲ့ Python library တွေသွင်းခြင်း
+# Python library များသွင်းခြင်း
 pip install -r requirements.txt
 
-# FFmpeg ကို အသစ်ပြန်ဒေါင်းပြီး နေရာချခြင်း
-rm -rf ffmpeg
+# FFmpeg ကို ဒေါင်းလုဒ်ဆွဲပြီး နေရာချခြင်း
 mkdir -p ffmpeg
 cd ffmpeg
 curl -L https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl.tar.xz | tar -xJ --strip-components=1
